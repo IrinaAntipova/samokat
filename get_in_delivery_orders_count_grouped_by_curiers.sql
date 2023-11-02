c1499eefeed4f3c303789c24e3325ee546eb0d19
Irina Antipova <iirinam91@gmail.com>
2023-11-02 08:53:35 +0300

    SQL & readme fix

--- get_in_delivery_orders_count_grouped_by_curiers.sql
+++ get_in_delivery_orders_count_grouped_by_curiers.sql
@@ -1,1 +1,6 @@
-SELECT c."firstName", COUNT(o.address) AS orders FROM "Couriers" as c LEFT JOIN "Orders" as o ON c.id=o."courierId" WHERE o."inDelivery" is true GROUP BY c."firstName";
+SELECT c.login,
+COUNT(o.id) AS “deliveryCount”
+FROM "Couriers" as c
+LEFT JOIN "Orders" as o ON c.id=o."courierId"
+WHERE o."inDelivery" is true
+GROUP BY c.login;
